1dc36c9b2ff17c732daa0b5ed746308e4799880b
chengqy <cqy370140794@163.com>
2020-11-20 13:14:15 +0800

    修改包名  v1.0

--- src/main/java/com/yaxin/Application.java
+++ src/main/java/com/yaxin/Application.java
@@ -1,4 +1,4 @@
-package com.baizhi;
+package com.yaxin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/yaxin/SpringRedisTest.java
+++ src/test/java/com/yaxin/SpringRedisTest.java
@@ -1,4 +1,4 @@
-package com.baizhi;
+package com.yaxin;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
