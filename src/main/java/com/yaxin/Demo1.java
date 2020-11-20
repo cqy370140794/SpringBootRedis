5a7812b10b57a13fd6f20ea2a385a8e731247bca
chengqy <cqy370140794@163.com>
2020-11-20 13:20:34 +0800

    添加测试demo  v2.0

---
+++ src/main/java/com/yaxin/Demo1.java
@@ -0,0 +1,12 @@
+package com.yaxin;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        for (int i = 0; i < a.length; i++) {
+            int i1 = a[i];
+            System.out.println(i1);
+        }
+    }
+}
